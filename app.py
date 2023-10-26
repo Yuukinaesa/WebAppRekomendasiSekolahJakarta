ad629d2bbf0baa6a57d86ee6537f7504d21bf408
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2023-10-26 16:24:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,6 @@ if mata_pelajaran:
 
 hide_streamlit_style = """
             <style>
-            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
